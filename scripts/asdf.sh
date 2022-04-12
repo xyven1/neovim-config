0ad5cd84f024040c93eea5e3b0dbcacddd0ddceb
Xyven <35360746+Xyven1@users.noreply.github.com>
2022-04-12 13:26:32 -0400

    Update asdf.sh

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -3,4 +3,9 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
 echo $'\n# asdf config' >> ~/.bashrc
 echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
-source ~/.bashrc
+. ~/.bashrc
+
+for value in neovim direnv fzf gitui rust lua lua-language-server golang erland elixir nodejs haskell
+do
+    asdf plugin-add $value
+done
