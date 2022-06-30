b16627f15a5578fbbdecdaa531c9046416097ee8
Xyven1 <maritimus987@gmail.com>
2022-06-29 22:48:51 -0400

    fixed signature bug

--- lua/config/signature.lua
+++ lua/config/signature.lua
@@ -18,14 +18,14 @@ require "lsp_signature".setup({
   -- this setting will be helpful if you do not want the PUM and floating win overlap
 
   floating_window_off_x = 1, -- adjust float windows x position.
-  floating_window_off_y = 1, -- adjust float windows y position.
+  floating_window_off_y = 0, -- adjust float windows y position.
 
 
   fix_pos = false,  -- set to true, the floating window will not auto-close until finish all parameters
   hint_enable = true, -- virtual hint enable
   hint_prefix = "🐼 ",  -- Panda for parameter
   hint_scheme = "String",
-  hi_parameter = "IncSearch", -- how your parameter will be highlight
+  hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
   max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
                    -- to view the hiding contents
   max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
