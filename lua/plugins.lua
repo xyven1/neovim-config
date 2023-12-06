d1a3d1496f8f15b05903186b503e652e8e56e70d
Xyven1 <maritimus987@gmail.com>
2023-12-06 14:19:03 -0500

    vscode transparent only for wezterm

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -352,26 +352,27 @@ require('lazy').setup(
     {
       'Mofiqul/vscode.nvim',
       config = function()
-        local c = require('vscode.colors').get_colors()
-        require('vscode').setup({
-          transparent = true,
+        local transparent = vim.env.TERM == 'wezterm'
+        local config = {
+          transparent = transparent,
           italic_comments = true,
-          disable_nvimtree_bg = true,
+          disable_nvimtrre_bg = transparent,
           group_overrides = {
             DapBreakpoint = { ctermbg = 0, fg = '#bf321d' },
             DapStopped = { ctermbg = 0, fg = '#ffcc00' },
             DapStoppedLine = { ctermbg = 0, bg = '#4b4b26' },
             DiagnosticUnnecessary = { link = 'NonText' },
             DiffviewDiffDeleteDim = { link = 'NonText' },
-            NotifyBackground = { ctermbg = 0, bg = '#000000' },
+            NotifyBackground = { ctermbg = 0, bg = '#1e1e1e' },
             BracketHighlighting0 = { fg = '#ffd700' },
             BracketHighlighting1 = { fg = '#da70d6' },
             BracketHighlighting2 = { fg = '#179fff' },
-            SagaNormal = { ctermbg = 0, bg = 'NONE' },
-            SagaNormalFloat = { ctermbg = 0, bg = 'NONE' },
+            NormalFloat = { ctermbg = 0, bg = 'NONE' },
+            StatusLine = { ctermbg = 0, bg = 'NONE' },
             Pmenu = { ctermbg = 0, bg = 'NONE' },
           },
-        })
+        }
+        require('vscode').setup(config)
         require('vscode').load()
       end,
       priority = 1000
