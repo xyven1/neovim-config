a1e46e2721bf03593d2a99f3f069edc6320c0185
xyven1 <git@xyven.dev>
2024-04-14 16:43:28 -0400

    fix comment.nvim

--- lua/plugins/augmentation.lua
+++ lua/plugins/augmentation.lua
@@ -53,14 +53,14 @@ return {
   {
     'numToStr/Comment.nvim',
     dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
-    event = "BufEnter",
+    lazy = false,
     config = function()
+      require('Comment').setup()
       vim.g.skip_ts_context_commentstring_module = true
       return {
         pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
       }
     end,
-    opts = {}
   },
   {
     'folke/todo-comments.nvim',
