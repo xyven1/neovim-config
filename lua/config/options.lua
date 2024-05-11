3f356669a83dc0189da59b9e43b26c4f0ef0a83d
xyven1 <git@xyven.dev>
2024-05-11 02:01:03 -0400

    return term colors

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,7 +10,25 @@ vim.o.termguicolors = true  -- use terminal colors
 vim.o.pumblend = 30         -- popup menu transparency
 vim.o.mouse = ''            -- disable mouse support
 vim.g.mapleader = " "
+
+-- neovide settings
 vim.g.neovide_transparency = 0.9
+vim.g.terminal_color_0 = '#1e1e1e'
+vim.g.terminal_color_1 = '#f44747'
+vim.g.terminal_color_2 = '#608b4e'
+vim.g.terminal_color_3 = '#dcdcaa'
+vim.g.terminal_color_4 = '#569cd6'
+vim.g.terminal_color_5 = '#c678dd'
+vim.g.terminal_color_6 = '#56b6c2'
+vim.g.terminal_color_7 = '#d4d4d4'
+vim.g.terminal_color_8 = '#808080'
+vim.g.terminal_color_9 = '#f44747'
+vim.g.terminal_color_10 = '#608b4e'
+vim.g.terminal_color_11 = '#dcdcaa'
+vim.g.terminal_color_12 = '#569cd6'
+vim.g.terminal_color_13 = '#c678dd'
+vim.g.terminal_color_14 = '#56b6c2'
+vim.g.terminal_color_15 = '#d4d4d4'
 vim.o.guifont = 'JetBrainsMono Nerd Font:h12'
 
 vim.opt.fillchars:append { diff = "╱" }
