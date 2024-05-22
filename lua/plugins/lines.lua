006acd7b0ea617ee4fea8364684ef3f42a80542d
xyven1 <git@xyven.dev>
2024-05-22 11:55:26 -0400

    clean up lines config

--- lua/plugins/lines.lua
+++ lua/plugins/lines.lua
@@ -1,21 +1,15 @@
 return {
   {
     'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'arkav/lualine-lsp-progress' },
     config = function()
       vim.opt.showmode = false
       require('lualine').setup {
         options = {
-          icons_enabled = true,
-          theme = 'auto',
-          component_separators = { left = '', right = '' },
-          section_separators = { left = '', right = '' },
-          disabled_filetypes = {},
-          always_divide_middle = true,
           globalstatus = true,
         },
         sections = {
-          lualine_a = { 'mode',
-          },
+          lualine_a = { 'mode', },
           lualine_b = { 'branch', 'diff', 'diagnostics' },
           lualine_c = { 'lsp_progress' },
           lualine_x = { 'overseer',
@@ -34,14 +28,11 @@ return {
           lualine_c = {},
           lualine_x = {},
           lualine_y = {},
-          lualine_z = { 'location' }
+          lualine_z = {}
         },
-        tabline = {},
-        extensions = {}
       }
     end,
     event = 'VeryLazy',
-    dependencies = { 'nvim-tree/nvim-web-devicons', 'arkav/lualine-lsp-progress' },
   },
   {
     'akinsho/bufferline.nvim',
