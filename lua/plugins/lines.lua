a098158814380e67a05336911fba0db46bdc9374
xyven1 <git@xyven.dev>
2024-06-06 01:26:39 -0400

    fix buffline weirdness

--- lua/plugins/lines.lua
+++ lua/plugins/lines.lua
@@ -63,7 +63,7 @@ return {
   },
   {
     'akinsho/bufferline.nvim',
-    event = 'VeryLazy',
+    event = 'LazyFile',
     opts = {
       options = {
         diagnostics = 'nvim_lsp',
@@ -91,7 +91,7 @@ return {
     config = function(_, opts)
       require("bufferline").setup(opts)
       -- Fix bufferline when restoring a session
-      vim.api.nvim_create_autocmd("BufAdd", {
+      vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
         callback = function()
           vim.schedule(function()
             pcall(nvim_bufferline)
