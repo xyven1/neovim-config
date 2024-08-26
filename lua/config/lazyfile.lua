a6c54a48aed091e3f051c1fae05bd59adec36208
xyven1 <git@xyven.dev>
2024-08-26 17:09:47 -0400

    update lazyfile

--- lua/config/lazyfile.lua
+++ lua/config/lazyfile.lua
@@ -1,60 +1,43 @@
+---@class lazyvim.util.plugin
 local M = {}
+
+M.lazy_file_events = { "BufReadPost", "BufNewFile", "BufWritePre" }
+
 function M.setup()
+  M.lazy_file()
+end
+
+function M.lazy_file()
+  -- This autocmd will only trigger when a file was loaded from the cmdline.
+  -- It will render the file as quickly as possible.
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    once = true,
+    callback = function(event)
+      -- Skip if we already entered vim
+      if vim.v.vim_did_enter == 1 then
+        return
+      end
+
+      -- Try to guess the filetype (may change later on during Neovim startup)
+      local ft = vim.filetype.match({ buf = event.buf })
+      if ft then
+        -- Add treesitter highlights and fallback to syntax
+        local lang = vim.treesitter.language.get_lang(ft)
+        if not (lang and pcall(vim.treesitter.start, event.buf, lang)) then
+          vim.bo[event.buf].syntax = ft
+        end
+
+        -- Trigger early redraw
+        vim.cmd([[redraw]])
+      end
+    end,
+  })
+
   -- Add support for the LazyFile event
   local Event = require("lazy.core.handler.event")
 
-  -- We'll handle delayed execution of events ourselves
-  Event.mappings.LazyFile = { id = "LazyFile", event = "User", pattern = "LazyFile" }
+  Event.mappings.LazyFile = { id = "LazyFile", event = M.lazy_file_events }
   Event.mappings["User LazyFile"] = Event.mappings.LazyFile
-
-  local events = {} ---@type {event: string, buf: number, data?: any}[]
-
-  local done = false
-  local function load()
-    if #events == 0 or done then
-      return
-    end
-    done = true
-    vim.api.nvim_del_augroup_by_name("lazy_file")
-
-    ---@type table<string,string[]>
-    local skips = {}
-    for _, event in ipairs(events) do
-      skips[event.event] = skips[event.event] or Event.get_augroups(event.event)
-    end
-
-    vim.api.nvim_exec_autocmds("User", { pattern = "LazyFile", modeline = false })
-    for _, event in ipairs(events) do
-      if vim.api.nvim_buf_is_valid(event.buf) then
-        Event.trigger({
-          event = event.event,
-          exclude = skips[event.event],
-          data = event.data,
-          buf = event.buf,
-        })
-        if vim.bo[event.buf].filetype then
-          Event.trigger({
-            event = "FileType",
-            buf = event.buf,
-          })
-        end
-      end
-    end
-    vim.api.nvim_exec_autocmds("CursorMoved", { modeline = false })
-    events = {}
-  end
-
-  -- schedule wrap so that nested autocmds are executed
-  -- and the UI can continue rendering without blocking
-  load = vim.schedule_wrap(load)
-
-  vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile", "BufWritePre" }, {
-    group = vim.api.nvim_create_augroup("lazy_file", { clear = true }),
-    callback = function(event)
-      table.insert(events, event)
-      load()
-    end,
-  })
 end
 
 return M
