5a491837710fe72bebc4cb5e9c9a6fa0d6aada72
xyven1 <git@xyven.dev>
2024-11-13 12:07:17 -0500

    add more default formatters

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -74,7 +74,16 @@ return {
           end
         end,
         cpp = { 'astyle' },
-        rust = { 'rustfmt' }
+        rust = { 'rustfmt' },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        html = { "prettierd", "prettier", stop_after_first = true },
+        css = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
+        svelte = { "prettierd", "prettier", stop_after_first = true },
+        vue = { "prettierd", "prettier", stop_after_first = true },
       }
     },
     keys = {
