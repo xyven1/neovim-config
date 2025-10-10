b32c454db9cb3d1b9dbdbb7b09bbe26b83fa8636
xyven1 <git@xyven.dev>
2025-10-10 14:54:57 -0600

    fix lsp formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -101,7 +101,7 @@ return {
         function()
           require('conform').format({
             async = true,
-            lsp_fallback = true,
+            lsp_format = "fallback",
           })
         end,
         desc = 'Format buffer',
