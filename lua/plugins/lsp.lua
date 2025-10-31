2bb0dbcdcf7f02d6d29b03f4d1e6f65981c7f699
xyven1 <git@xyven.dev>
2025-10-31 12:15:00 -0600

    bump inputs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -409,7 +409,7 @@ return {
   -- Language Specific
   {
     'mrcjkb/rustaceanvim',
-    version = '^5', -- Recommended
+    version = '^6', -- Recommended
     lazy = false,   -- This plugin is already lazy
   }
 }
